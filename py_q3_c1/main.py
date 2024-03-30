714b58008440efa24150f80a8d851c3333178587
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-03-30 06:24:10 +0500

    Remove unused code and add sum calculation

--- py_q3_c1/main.py
+++ py_q3_c1/main.py
@@ -1,19 +1,27 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
 
-app = FastAPI(title="Hello World API", 
-    version="0.0.1",
-    servers=[
-        {
-            "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
-            "description": "Development Server"
-        }
-        ])
+# app = FastAPI(title="Hello World API", 
+#     version="0.0.1",
+#     servers=[
+#         {
+#             "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
+#             "description": "Development Server"
+#         }
+#         ])
 
 
-@app.get("/")
-def read_root():
-    return {"Message": "Hello World"};
+# @app.get("/")
+# def read_root():
+#     return {"Message": "Hello World"};
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str = None):
+#     return {"item_id": item_id, "q": q}
+
+
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+sum = a + b
+print(sum)
+
